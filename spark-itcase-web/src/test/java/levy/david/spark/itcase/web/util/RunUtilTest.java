2ee5204187fab3d13acf2941df88a27aaf74bfb1
David Levy <david.g.levy@gmail.com>
2016-05-22 16:02:56 +1000

    Added provision so test won't fail on linux.

--- spark-itcase-web/src/test/java/levy/david/spark/itcase/web/util/RunUtilTest.java
+++ spark-itcase-web/src/test/java/levy/david/spark/itcase/web/util/RunUtilTest.java
@@ -2,21 +2,36 @@ package levy.david.spark.itcase.web.util;
 
 import static org.junit.Assert.assertEquals;
 
+import org.apache.commons.lang.SystemUtils;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class RunUtilTest {
 	
 	@Test(expected=Exception.class)
 	public void testNonExistent() throws Exception {
 		RunUtil util = new RunUtil();
+		if (SystemUtils.IS_OS_WINDOWS) {
+			int output = util.runCommand("C:\\Temp\\hello_world_2.bat");			
+		} else if (SystemUtils.IS_OS_LINUX){
+			int output = util.runCommand("/usr/bin/echo_2");						
+		} else {
+			fail("No system tests for operating system [" + SystemUtils.OS_NAME + "]");
+		}
 		
-		int output = util.runCommand("C:\\Temp\\hello_world_2.bat");
 	}
 	@Test
 	public void testExists() throws Exception {
 		RunUtil util = new RunUtil();
 		
-		int output = util.runCommand("C:\\Temp\\hello_world.bat");
-		assertEquals(0, output);
+		if (SystemUtils.IS_OS_WINDOWS) {
+			int output = util.runCommand("C:\\Temp\\hello_world.bat");
+			assertEquals(0, output);
+		} else if (SystemUtils.IS_OS_LINUX){
+			int output = util.runCommand("/usr/bin/echo");
+			assertEquals(0, output);
+		} else {
+			fail("No system tests for operating system [" + SystemUtils.OS_NAME + "]");
+		}
 	}
 }
