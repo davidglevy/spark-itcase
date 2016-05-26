1493dcd1d358604546641e529800712e85485509
David Levy <dlevy@cloudera.com>
2016-05-26 14:39:03 +1000

    Added required ASF banner to the code

--- spark-itcase-web/src/main/java/levy/david/spark/itcase/web/controller/ArtifactController.java
+++ spark-itcase-web/src/main/java/levy/david/spark/itcase/web/controller/ArtifactController.java
@@ -1,5 +1,24 @@
 package levy.david.spark.itcase.web.controller;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -28,11 +47,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.HttpServletBean;
 import org.springframework.web.servlet.ModelAndView;
-
-/*
- * author: Crunchify.com
- * 
- */
 
 @Controller
 public class ArtifactController {
--- spark-itcase-web/src/main/java/levy/david/spark/itcase/web/controller/SparkJobController.java
+++ spark-itcase-web/src/main/java/levy/david/spark/itcase/web/controller/SparkJobController.java
@@ -1,33 +1,39 @@
 package levy.david.spark.itcase.web.controller;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.ServletException;
 
 import levy.david.spark.itcase.core.LocalRepository;
-import levy.david.spark.itcase.web.domain.LoadParams;
 import levy.david.spark.itcase.web.domain.RunParams;
 import levy.david.spark.itcase.web.domain.RunResult;
 import levy.david.spark.itcase.web.util.RunUtil;
 
-import org.apache.commons.exec.CommandLine;
-import org.apache.commons.exec.DefaultExecuteResultHandler;
-import org.apache.commons.exec.DefaultExecutor;
-import org.apache.commons.exec.ExecuteStreamHandler;
-import org.apache.commons.exec.ExecuteWatchdog;
-import org.apache.commons.exec.Executor;
-import org.apache.commons.exec.LogOutputStream;
-import org.apache.commons.exec.PumpStreamHandler;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
